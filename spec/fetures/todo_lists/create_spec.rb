a8c1bbe8d813c45b10d986dc1ed295b91633ef88
Andre Foresti <aforesti@gmail.com>
2015-03-11 22:36:56 -0300

    removidos hints

--- spec/fetures/todo_lists/create_spec.rb
+++ spec/fetures/todo_lists/create_spec.rb
@@ -3,12 +3,32 @@ require 'rails_helper'
 describe "Creating todo lists" do
   
   it "should create a todo list" do
-  	visit "/todo_lists/new"
+  	expect(TodoList.count).to eq(0)
 
-  	click_button "Create Todo list"
+  	visit "/todo_lists"
 
+  	click_link "New Todo list"  	
   	expect(page).to have_content('New Todo List')
+
+    fill_in "Title", with: "My TodoList"
+     
+    click_button "Create Todo list"
+
+  	expect(TodoList.count).to eq(1)
   end
 
+  it "should not create a todo list withou a title" do
+  	visit "/todo_lists"
+
+  	click_link "New Todo list"  	
+  	expect(page).to have_content('New Todo List')
+
+    fill_in "Title", with: ""
+     
+    click_button "Create Todo list"
+
+
+  	expect(page).to have_content("error")
+  end
 	
 end
